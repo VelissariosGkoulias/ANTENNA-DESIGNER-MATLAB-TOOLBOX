36a3d19ae5a82b9afe47b7d775cd20a883ca8985
Velissarios Gkoulias <velissariosgkoulias@gmail.com>
2025-04-05 21:14:57 +0300

    feat: new script for bem method and new material
    material based on mathworks website for rcs values

---
+++ bem.m
@@ -0,0 +1,22 @@
+% Compare built-in cube, cylinder, sphere
+cube = antennaRectangle('Length', 1, 'Width', 1);
+cyl = antennaCylinder('Radius', 0.5, 'Height', 1);
+sph = antennaSphere('Radius', 0.5);
+
+objs = {cube, cyl, sph};
+labels = {'Cube', 'Cylinder', 'Sphere'};
+az = 0:1:360;
+freq = 1e9;
+
+figure;
+hold on;
+for i = 1:length(objs)
+    rcs = radarCrossSection(objs{i}, freq, 'Azimuth', az, 'Elevation', 0);
+    plot(az, 10*log10(rcs), 'LineWidth', 2);
+end
+hold off;
+xlabel('Azimuth (°)');
+ylabel('RCS (dBsm)');
+title('Built-in Shape RCS Comparison');
+legend(labels);
+grid on;
